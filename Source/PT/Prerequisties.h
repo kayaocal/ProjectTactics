ba2240462b295f6401c0b1a78936da6b42342ce8
Enes Kaya ÖCAL <ekayaocal@hotmail.com>
2020-05-30 16:24:22 +0300

    Prerequisties.h for Logging macros

---
+++ Source/PT/Prerequisties.h
@@ -0,0 +1,5 @@
+﻿#include "CoreMinimal.h"
+
+#define LOG(txt, ...) UE_LOG(LogTemp, Log, TEXT(txt), __VA_ARGS__)
+#define LOG_W(txt, ...) UE_LOG(LogTemp, Warning, TEXT(txt), __VA_ARGS__)
+#define LOG_ERR(txt, ...) UE_LOG(LogTemp, Error, TEXT(txt), __VA_ARGS__)
